f5b17dba429bccb98f14ff275ca2f585970546c3
JON HOUSE <J_HOUSE974@YAHOO.CA>
2015-01-31 22:46:55 -0500

    setup main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,42 @@
 class MainController < ApplicationController
+	 protect_from_forgery with: :exception
+
+	 def index
+	 end
+
+	 def blog
+	 end
+
+	 def press
+	 end
+
+	 def art
+	 end
+
+	 def finishedworks
+	 end
+
+	 def gallery
+	 end
+
+	 def performance
+	 end
+
+	 def publications
+	 end
+
+	 def recordings
+	 end
+
+	 def music
+
+
+	 def awardsandpress
+	 end
+
+	 def about
+	 end
+
+
+
 end
